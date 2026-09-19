a1da5342bee42fa7c6e0c36d0e09830727249315
Nicholas J Koonce <nick@kfe.net>
2026-09-18 21:51:55 -0700

    chore: release v1.1.2

--- youract-content-library/templates/opportunity-details.php
+++ youract-content-library/templates/opportunity-details.php
@@ -20,19 +20,7 @@ $stage_labels = array(
 );
 $stage = (string) ( $opportunity['stage'] ?? 'exploring' );
 ?>
-<section class="youract-details youract-opportunity-details" aria-labelledby="<?php echo esc_attr( (string) $opportunity['heading_id'] ); ?>">
-	<h2 id="<?php echo esc_attr( (string) $opportunity['heading_id'] ); ?>"><?php esc_html_e( 'Opportunity Details', 'youract-content-library' ); ?></h2>
-	<?php if ( ! empty( $opportunity['subtitle'] ) ) : ?>
-		<p><?php echo esc_html( (string) $opportunity['subtitle'] ); ?></p>
-	<?php endif; ?>
-	<dl class="youract-meta-list">
-		<dt><?php esc_html_e( 'Stage', 'youract-content-library' ); ?></dt>
-		<dd><?php echo esc_html( $stage_labels[ $stage ] ?? $stage_labels['exploring'] ); ?></dd>
-		<?php if ( ! empty( $opportunity['featured'] ) ) : ?>
-			<dt><?php esc_html_e( 'Featured', 'youract-content-library' ); ?></dt>
-			<dd><?php esc_html_e( 'Yes', 'youract-content-library' ); ?></dd>
-		<?php endif; ?>
-	</dl>
+<section class="youract-details youract-opportunity-details" aria-label="<?php esc_attr_e( 'Opportunity action', 'youract-content-library' ); ?>">
 	<?php if ( ! empty( $opportunity['cta_label'] ) && ! empty( $opportunity['cta_url'] ) ) : ?>
 		<p><a href="<?php echo esc_url( (string) $opportunity['cta_url'] ); ?>"><?php echo esc_html( (string) $opportunity['cta_label'] ); ?></a></p>
 	<?php endif; ?>
